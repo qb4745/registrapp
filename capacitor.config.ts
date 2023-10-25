3c8d07ab60d4d028c247bf0a424951de327f849f
Barriagada666 <bori.arriagada@duocuc.cl>
2023-10-25 15:51:50 -0300

    splash

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,12 +1,24 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'registrapp',
-  webDir: 'www',
-  server: {
-    androidScheme: 'https'
-  }
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: true,
+      launchFadeOutDuration: 3000,
+      backgroundColor: "#ffffffff",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: true,
+      androidSpinnerStyle: "large",
+      iosSpinnerStyle: "small",
+      spinnerColor: "#999999",
+      splashFullScreen: true,
+      splashImmersive: true,
+      layoutName: "launch_screen",
+      useDialog: true,
+    },
+  },
 };
 
 export default config;
