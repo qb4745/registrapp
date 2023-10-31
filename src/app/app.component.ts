b06e25788f7641ba7916c6e247dae790f7c58d63
Jaime Vicencio <jai.vicencio@duocuc.cl>
2023-10-31 03:48:45 -0300

    app redirect

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
-import { Component, EnvironmentInjector, inject } from '@angular/core';
+import { Component, EnvironmentInjector, NgZone, inject } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { AuthService } from './services/auth.service';
+import { App, URLOpenListenerEvent } from '@capacitor/app';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +14,23 @@ import { CommonModule } from '@angular/common';
 })
 export class AppComponent {
   public environmentInjector = inject(EnvironmentInjector);
+  constructor(private zone: NgZone, private router: Router, private authService: AuthService) {
+    this.setupListener()
+  }
 
-  constructor() {}
+  setupListener() {
+    App.addListener('appUrlOpen', async (data: URLOpenListenerEvent) => {
+      console.log('app opened with URL: ', data)
+
+      const openUrl = data.url
+      const access = openUrl.split('#access_token=').pop().split('&')[0]
+      const refresh = openUrl.split('&refresh_token=').pop().split('&')[0]
+
+      await this.authService.setSession(access, refresh)
+
+      this.zone.run(() => {
+        this.router.navigateByUrl('/groups', { replaceUrl: true })
+      })
+    })
+  }
 }
